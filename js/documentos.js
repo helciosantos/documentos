1642ee06e5cf30266d20c7ed536a0164103b0a24
Hélcio Lopes <helciokarman@gmail.com>
2020-01-27 21:41:36 -0300

    Conectando ao firebase

--- js/documentos.js
+++ js/documentos.js
@@ -4,6 +4,15 @@ function search() {
     validation(cpf);
 }
 
-function validation(cpfValeu){
-    document.getElementById('tituloDocumentos').innerHTML - 'Certificados de: '+cpfValeu;
+function validation(cpfValue){
+    document.getElementById('busca').setAttribute("class", "ocultar");
+    document.getElementById('resultado').removeAttribute("class", "ocultar");
+    document.getElementById('tituloDocumentos').innerHTML = 'Certificados de: '+cpfValue;
+
+}
+
+function back(){
+    document.getElementById('busca').removeAttribute("class", "ocultar");
+    document.getElementById('resultado').setAttribute("class", "ocultar");
+    document.getElementById('inputCPF').value = '';
 }
---
+++ js/initFirebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyD-z8dCCSFtrJng-y1YZ0kws6549uIN5KE",
+    authDomain: "documentos-b69dc.firebaseapp.com",
+    databaseURL: "https://documentos-b69dc.firebaseio.com",
+    projectId: "documentos-b69dc",
+    storageBucket: "documentos-b69dc.appspot.com",
+    messagingSenderId: "831060922372",
+    appId: "1:831060922372:web:9227ee779a71ad25bb6551"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
