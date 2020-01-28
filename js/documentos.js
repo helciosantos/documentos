99218230711b1d4995f3f4834f0183f65f22e152
Hélcio Lopes <helciokarman@gmail.com>
2020-01-28 20:48:39 -0300

    Adicionando firebase e busca da pasta por CPF

--- js/documentos.js
+++ js/documentos.js
@@ -1,14 +1,29 @@
 function search() {
     var cpf = document.getElementById('inputCPF').value;
-    console.log(cpf);
+    // console.log(cpf);
     validation(cpf);
 }
 
 function validation(cpfValue){
+    var storage = firebase.storage();
+    //Retorna uma promesses que será processada
+    storage.ref().child(cpfValue).listAll().then(function(todosArquivos){
+        if(todosArquivos.items.length >= 1){
+            next(cpfValue); 
+        } else{
+            alert('CPF não cadastrado');
+        }   
+    }).catch(function(error){
+        console.log('ERRO', error);
+    });
+    
+
+}
+
+function next(cpfValue){
     document.getElementById('busca').setAttribute("class", "ocultar");
     document.getElementById('resultado').removeAttribute("class", "ocultar");
     document.getElementById('tituloDocumentos').innerHTML = 'Certificados de: '+cpfValue;
-
 }
 
 function back(){
